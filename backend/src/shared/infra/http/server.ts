cf5dbb6153b53525f0a60e122f77c335c2339faa
pedr0fontoura <pedrofontoura@id.uff.br>
2021-06-14 08:54:03 -0300

    fix(backend): allow cors

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 
+import cors from 'cors';
 import express from 'express';
 
 import routes from './routes';
@@ -8,6 +9,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
