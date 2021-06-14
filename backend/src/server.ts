c983fdf333e96a0972645a8aae1021d7d59750e5
pedr0fontoura <pedrofontoura@id.uff.br>
2021-06-14 01:28:40 -0300

    feat(backend): initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello World' });
+});
+
+app.listen(3333, () => console.log('Server is running on port 3333'));
